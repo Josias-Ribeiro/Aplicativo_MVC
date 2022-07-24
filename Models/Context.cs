60b9c519a6f2f78c6439051ef92d0a6959c2034a
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2022-07-24 03:37:52 -0300

    APMVC - Ajustando Model produto

--- Models/Context.cs
+++ Models/Context.cs
@@ -9,6 +9,8 @@ namespace Aplicativo_MVC.Models
     public class Context : DbContext
     {
         public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
